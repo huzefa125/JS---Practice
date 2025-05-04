05554db618d43d4bc44fa89e9b0c8bf7f325b488
huzefa125 <huzafahuzafa63@gmail.com>
2025-05-04 15:17:05 +0000

    container Config done

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,31 @@
+// Premitive
+
+// types of data : number,string,Bollean,null,undefined,symbol,bigint
+const number =100;
+const numbers = 100.21;
+
+const isLoggedIn = true
+
+const name = "huzefa";
+let useremails; 
+
+const id = Symbol('123')
+const aid = Symbol('123')
+
+console.log(id === aid)
+const bigNumber = 544668383884n
+
+// Non premitive 
+
+// type of data : objects , array, function
+
+array = ["Huzefa","Hiran","Salaman","Yasin"];
+
+let Obj = {
+    name :"Huzefa",
+    age : 18,
+}
+
+function add(){
+    console.log("Hello my Javascript")
+}
