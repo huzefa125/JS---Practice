ce74086d663a0d60847c89f9697d8d0a019e8a17
huzefa125 <huzafahuzafa63@gmail.com>
2025-05-04 07:02:53 +0000

    Container config done

---
+++ 01_basic/variables.js
@@ -0,0 +1,14 @@
+ const BankId = 23018501210236  
+ let accountEmail = "huzefa@google.com"
+ var accountPassword = "Huzefa123"
+ accountCity = "ahmedabad"
+let accountState
+
+
+//BankId = 1233 const not allowed
+accountEmail ="test" // Allowed //
+
+// Avoid the use of var 
+
+console.table([BankId,accountEmail,accountPassword,accountCity,accountState]);
+ 
