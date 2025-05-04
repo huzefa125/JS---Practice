3037eb387430c4b7d8f3587e009beb753e4e877a
huzefa125 <huzafahuzafa63@gmail.com>
2025-05-04 14:12:10 +0000

    Container config done

---
+++ 01_basic/typeof.js
@@ -0,0 +1,25 @@
+let name = "huzefa";
+
+//console.log(name)
+//console.log(typeof(name))
+
+let score = 33;
+console.log(score);
+console.log(typeof(score))
+
+// "33" => 33 string to number
+// "33abc" => NaN Not a number
+// true => 1 and flase => 0
+
+
+let login = "huzefa";
+let loggin = Boolean(login);
+console.log(loggin)
+console.log(typeof(loggin))
+
+// name => true
+
+let number = 33
+let string = String(number)
+console.log(string)
+console.log(typeof(string))
