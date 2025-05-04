7cf13028494927dc8a9fc20156bafccad2131c6a
huzefa125 <huzafahuzafa63@gmail.com>
2025-05-04 14:31:05 +0000

    Container congif done

---
+++ 01_basic/conversion.js
@@ -0,0 +1,21 @@
+let value = 3
+let negValue = - value
+console.log(negValue)
+
+// value ko negative kar dega
+
+// console.log(2 + 2)
+// console.log(2 * 2)
+// console.log(3 / 2)
+// console.log(2 % 2)
+// console.log(2 ** 2)
+
+str1 = "Huzefa";
+str2 = "Shaikh";
+
+let name = (str1 + str2)
+console.log(name)
+
+console.log("1" + 2) 
+console.log(1 + "2")
+console.log(1 + 2+"2")
