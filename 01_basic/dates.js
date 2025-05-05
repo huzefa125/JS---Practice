b804068134ab5f8542d78f594260456fe7b6682d
huzefa125 <huzafahuzafa63@gmail.com>
2025-05-05 17:38:24 +0000

    Container config done

---
+++ 01_basic/dates.js
@@ -0,0 +1,25 @@
+let Dates = new Date
+
+// console.log(Dates);
+// console.log(Dates.toString());
+// console.log(Dates.toLocaleString());
+// console.log(Dates.toDateString());
+// console.log(typeof(Dates));
+
+// let myCreateDates = new Date(2023,2,21,12,34)
+// console.log(myCreateDates.toLocaleString());
+
+let myCreateDate = new Date("2005-08-31")
+// console.log(myCreateDate.toLocaleString());
+
+// Time
+let MyTime = Date.now()
+// console.log(MyTime);
+// console.log(myCreateDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+MyTime.toLocaleString('default'{
+    weekday : "Long"
+})
+
+console.log(MyTime);
