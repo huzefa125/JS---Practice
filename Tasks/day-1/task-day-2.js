433e0cac39efc5d7f1913362cdfcf3d4ed7b2b5b
huzefa125 <huzafahuzafa63@gmail.com>
2025-05-05 13:22:09 +0000

    Container config done

---
+++ Tasks/day-1/task-day-2.js
@@ -0,0 +1,57 @@
+// Task 1: Addition
+let a = 10, b = 5;
+console.log("Addition:", a + b);
+
+// Task 2: Subtraction
+console.log("Subtraction:", a - b);
+
+// Task 3: Multiplication
+console.log("Multiplication:", a * b);
+
+// Task 4: Division
+console.log("Division:", a / b);
+
+// Task 5: Remainder (Modulus)
+console.log("Remainder:", a % b);
+
+// Activity 2: Assignment Operators
+
+// Task 6: + operator ka use karke ek variable me number add karo aur result console par log karo.
+let x = 10
+x += 5
+console.log(x);
+
+// Task 7: - operator ka use karke ek variable se number subtract karo aur result console par log karo.
+let y = 10
+y -= 5
+console.log(y);
+
+// Activity 3: Comparison Operators
+// Task 8: Do numbers ko > aur < se compare karne ka program banao aur result console par log karo.
+let num1 = 10 
+let num2 = 20
+console.log( num1 > num2);
+console.log(num1 < num2);
+
+
+// Task 9: Do numbers ko > aur <= se compare karne ka program banao aur result console par log karo.
+console.log(num1 > num2);
+console.log(num1 <= num2);
+
+// Task 10: Do numbers ko == aur === se compare karne ka program banao aur result console par log karo.
+console.log(x == y);
+console.log(x === y);
+
+// Activity 4: Logical Operators
+// Task 11: && operator ka use karke do conditions ko combine karo aur result console par log karo.
+const age = 16
+const name = "huzefa"
+console.log(age == 18 && name == "huzefa");
+
+// Task 12: || operator ka use karke do conditions ko combine karo aur result console par log karo.
+
+console.log(age == 18 || name === "huzefa");
+
+// Task 13: ! operator ka use karke ek condition ko negate karo aur result console par log karo.
+let log = true
+console.log(!log);
