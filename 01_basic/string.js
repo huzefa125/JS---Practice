382fe7824ea5589d61c5401805ea463f32cf3faa
huzefa125 <huzafahuzafa63@gmail.com>
2025-05-05 07:29:05 +0000

    Container config done

---
+++ 01_basic/string.js
@@ -0,0 +1,32 @@
+const name = "Huzefa"
+const Age = 18
+
+// console.log(name + Age + "Value")
+
+// console.log(`My Name is ${name} and My Age is ${Age} `)
+
+const gameName = new String("Huzefaity")
+// console.log(gameName);
+// console.log(gameName.__proto__)
+// console.log(gameName[2]);
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+
+// const newString = gameName.substring(0,5)
+// // console.log(newString);
+
+// const anotherString = gameName.slice(1,7)
+// console.log(anotherString);
+
+// const newString = "  huzefa  "
+// console.log(newString)
+// console.log(newString.trim());
+
+const url = "https://huzefa.com//huzefa%63//"
+// console.log(url.replace('%','-'));
+
+//  console.log(url.includes('huzedfa'))
+
+console.log(url.split('//'));
