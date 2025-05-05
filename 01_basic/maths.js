069791155f3661da0a4c9f56904eac78cdc82435
huzefa125 <huzafahuzafa63@gmail.com>
2025-05-05 09:20:09 +0000

    Container config done

---
+++ 01_basic/maths.js
@@ -0,0 +1,33 @@
+// const number = 100
+// console.log(number);
+
+// const balance  = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+
+// const numbers =13.34655 
+// console.log(numbers.toPrecision(4));
+ 
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString());
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(2.34));
+// console.log(Math.ceil(2.32));
+// console.log(Math.floor(2.34));
+// console.log(Math.max(4,3,5,1));
+// console.log(Math.min(6,4,5,1));
+
+console.log(Math.random()* 10);
+console.log(Math.floor(Math.random()* 10));
+console.log(Math.floor(Math.random()* 10 + 1));
+
+const min =10
+const max = 20
+
+console.log(Math.floor( Math.random() * (max - min -1)));
