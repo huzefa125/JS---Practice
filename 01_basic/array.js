6668c8808add3daebca981bc8947f4d44de1f8b9
huzefa125 <huzafahuzafa63@gmail.com>
2025-05-06 08:04:06 +0000

    Container config done

---
+++ 01_basic/array.js
@@ -0,0 +1,51 @@
+// array
+// 0 based index
+
+
+const myArr = [1,2,3,"huzefa",true]
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(10)
+// myArr.shift()
+// console.log(myArr.indexOf(5));
+
+// console.log(myArr.includes(5));
+// console.log(myArr);
+
+// const mynewArr = myArr.join()
+// console.log(typeof mynewArr);
+
+// const my1 = myArr.slice(1,3)
+// console.log(myArr);
+// console.log(my1);
+
+// const my2 = myArr.splice(1,3)
+// console.log(myArr);
+// console.log(my2);
+
+const marvel = ["thor","ironman","hulk"]
+const dc = ["superman","batman","flash"]
+
+// marvel.push(dc)
+// console.log(marvel);
+// console.log(marvel[3][2]);
+
+// const all = marvel.concat(dc)
+// console.log(all);
+
+// const allhero = [...marvel,...dc]
+// console.log(allhero);
+
+
+// const another = [1,2,3,4,[5,6,[7,8],8],10]
+// const real = another.flat(Infinity)
+// console.log(real);
+
+// console.log(Array.isArray("huzefa"));
+// console.log(Array.from("huzefa"));
+
+let score1 = 100
+
+let score2 =200
+let score3 =300
+console.log(Array.of(score1,score2,score3));
