834b3ca647085f0510840e4f9dcd75bc61dbfaac
huzefa125 <huzafahuzafa63@gmail.com>
2025-05-07 14:04:44 +0000

    Container config done

---
+++ 01_basic/scope.js
@@ -0,0 +1,43 @@
+// let a = 300
+// if (true) {
+//     let a = 0;
+//     const b = 10;
+//     var c = 30
+//     console.log(a);
+    
+// }
+
+// console.log(a);
+// // console.log(b);
+// console.log(c);
+
+// function outer(){
+//     const username = "huzefa";
+//     function inner(){
+//         const email = "huzefa@gmail.com"
+//     }
+//     inner()
+// }
+
+
+if (true){
+    const username = "huzefa";
+    if (username === "huzefa"){
+        const website = "hello";
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+console.log(add(4));
+
+function add(num) {
+    return num + 1
+}
+
+// console.log(num);
+
+let num = function add(num) {
+    return num + 2
+}
+
