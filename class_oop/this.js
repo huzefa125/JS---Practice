7e3c54cf6b0430be63853ee21e37b425d39f974d
huzefa125 <huzafahuzafa63@gmail.com>
2025-05-12 15:14:18 +0000

    Container config done

---
+++ class_oop/this.js
@@ -0,0 +1,13 @@
+function setUsername(username){
+    this.username = username;
+    console.log("called")
+}
+
+function createUser(email,password){
+    setUsername.call(this,username)
+
+    this.email = email
+    this.password = password
+}
+const chai = new createUser("chai","huzefa@gmail.com","123")
+console.log(chai)
